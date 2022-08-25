3befb432af49f1dca5e318a303373c948818be56
hilmimuharromi <hilmi@mtn-fm.id>
2022-08-25 20:33:40 +0700

    cli pointer struct

---
+++ cli_closure_pointer_struct/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Person struct{ name string }
+
+var PrintPerson = func(idx int) {
+	listPerson := []*Person{
+		{name: "Nugraha Muthus"},
+		{name: "Bayu Fajar Sidik"},
+		{name: "I Putu Eka"},
+		{name: "Satrio Utomo"},
+		{name: "Agus Supriyatna"},
+		{name: "Barru Kurniawan"},
+		{name: "Hasanudin"},
+		{name: "Yudha Nugraha"},
+		{name: "Eka Muhendra"},
+		{name: "Fatur Ewing Fadh"},
+	}
+
+	fmt.Println("person:", listPerson[idx].name)
+}
+
+func main() {
+	idx := os.Args[1]
+	idxString, _ := strconv.Atoi(idx)
+	PrintPerson(idxString)
+}
