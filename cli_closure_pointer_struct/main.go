60b799bcbfac8cf1d1ef9b5d748da5ab5733c1c4
hilmimuharromi <hilmi@mtn-fm.id>
2022-08-25 20:39:52 +0700

    add alamat

--- cli_closure_pointer_struct/main.go
+++ cli_closure_pointer_struct/main.go
@@ -6,20 +6,25 @@ import (
 	"strconv"
 )
 
-type Person struct{ name string }
+type Person struct {
+	nama      string
+	alamat    string
+	pekerjaan string
+	alasan    string
+}
 
 var PrintPerson = func(idx int) {
 	listPerson := []*Person{
-		{name: "Nugraha Muthus"},
-		{name: "Bayu Fajar Sidik"},
-		{name: "I Putu Eka"},
-		{name: "Satrio Utomo"},
-		{name: "Agus Supriyatna"},
-		{name: "Barru Kurniawan"},
-		{name: "Hasanudin"},
-		{name: "Yudha Nugraha"},
-		{name: "Eka Muhendra"},
-		{name: "Fatur Ewing Fadh"},
+		{nama: "Nugraha Muthus", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Bayu Fajar Sidik", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "I Putu Eka", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Satrio Utomo", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Agus Supriyatna", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Barru Kurniawan", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Hasanudin", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Yudha Nugraha", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Eka Muhendra", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
+		{nama: "Fatur Ewing Fadh", alamat: "Jalan Kebon Sirih", pekerjaan: "Developer", alasan: "Nambah Ilmu"},
 	}
 
 	if idx > len(listPerson) {
@@ -27,7 +32,11 @@ var PrintPerson = func(idx int) {
 		return
 	}
 
-	fmt.Println("person:", listPerson[idx].name)
+	fmt.Println("nama:", listPerson[idx].nama)
+	fmt.Println("alamat:", listPerson[idx].alamat)
+	fmt.Println("Pekerjaan:", listPerson[idx].pekerjaan)
+	fmt.Println("alasan:", listPerson[idx].alasan)
+
 }
 
 func main() {
