c50dbe3becab7607e11d2df08678d991df0f2d17
hilmimuharromi <hilmi@mtn-fm.id>
2022-08-30 21:01:22 +0700

    add waitgroup

--- task_create_get_uer/main.go
+++ task_create_get_uer/main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"task_get_user/service"
 )
 
 func main() {
 	var db []*service.User
 	userSvc := service.NewUserService(db)
-	listUsers := []string{"Bayu", "Ahmed", "Mail", "Bintang"}
+	listUsers := []string{"Bayu", "Ahmed", "Mail", "Bintang", "kadek", "Anjas", "kevin", "hugo", "Syendi", "Nazera"}
+	var wg sync.WaitGroup
+	wg.Add(len(listUsers))
 	for _, v := range listUsers {
-		res := userSvc.Register(&service.User{Nama: v})
-		fmt.Println(res, "Berhasil didaftarkan")
+		go func(name string) {
+			res := userSvc.Register(&service.User{Nama: name})
+			fmt.Println(res, "Berhasil didaftarkan")
+			wg.Done()
+		}(v)
 	}
-	resUsers := userSvc.GetUser()
+	wg.Wait()
 
+	var wg1 sync.WaitGroup
+	resUsers := userSvc.GetUser()
+	wg1.Add(len(resUsers))
 	fmt.Println("-----------Hasil get user-------------")
 	for _, v := range resUsers {
-		fmt.Println(v.Nama)
+		go func(name string) {
+			fmt.Println(name)
+			wg1.Done()
+		}(v.Nama)
 	}
+	wg1.Wait()
 
 }
